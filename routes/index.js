61be43b56b4a01f07ae789841ae67eb5ad70a24c
Ahsan Ali <60280488+Phant0m-a@users.noreply.github.com>
2021-01-17 08:20:06 +0500

    Add files via upload
    first commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req,res)=>{
+    res.render('index')
+})
+router.get('/secret', (req,res) =>{
+    res.render('secret')
+})
+
+module.exports = router
